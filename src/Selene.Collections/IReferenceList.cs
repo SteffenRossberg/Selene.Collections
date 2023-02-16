881b8fa17450e59a6a8d1246ca6be867622cef2d
Steffen Roßberg <srossberg@gmx.net>
2023-02-16 04:56:14 +0100

    added RemoveAt and IndexOf

--- src/Selene.Collections/IReferenceList.cs
+++ src/Selene.Collections/IReferenceList.cs
@@ -5,5 +5,9 @@ namespace Selene.Collections
     public interface IReferenceList<T> : ICollection<T>
     {
         ref T this[int index] { get; }
+
+        void RemoveAt(int index);
+
+        int IndexOf(T item);
     }
 }
